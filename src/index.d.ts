6f14a6ce5cb28b86b9758da4dc5c923b3a896085
TV Quiz PhD <tvquizphd@gmail.com>
2022-09-24 11:59:33 -0400

    export project channel

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./projectSock";
+export * from "./projectChannel";
 export * from "./toNamespace";
 export * from "./toProject";
 export * from "./b64url";
--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./projectSock"), exports);
+__exportStar(require("./projectChannel"), exports);
 __exportStar(require("./toNamespace"), exports);
 __exportStar(require("./toProject"), exports);
 __exportStar(require("./b64url"), exports);
